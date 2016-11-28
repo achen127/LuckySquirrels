d58013424bf33d36740d36a093a1ba0f5c16ed66
Cherie Ruan <cherie_ruan@hotmail.com>
2016-11-27 21:55:07 -0800

    Complete all form tests for reset button, closes #8

--- kanye2020/src/App.test.js
+++ kanye2020/src/App.test.js
@@ -1,8 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import SignUpForm from './TeamSignUp';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import { shallow, mount } from 'enzyme';
+
+describe("Reset button", () => {
+
+  let wrapperForm;
+  let resetButton;
+
+  beforeEach(() => {
+    wrapperForm = mount(<SignUpForm />);
+    resetButton = wrapperForm.find('#resetButton');
+  });
+
+  it("should clear email form", () => {
+    const emailInput = wrapperForm.find('#email');
+    emailInput.simulate('change', { target: { value: 'a@a.com' } });
+    expect(emailInput.props().value).toEqual('a@a.com');
+    resetButton.simulate('click');
+    expect(emailInput.props().value).toEqual('');
+  });
+
+  it("should clear name form", () => {
+    const nameInput = wrapperForm.find('#name');
+    nameInput.simulate('change', { target: { value: 'Jane Doe' } });
+    expect(nameInput.props().value).toEqual('Jane Doe');
+    resetButton.simulate('click');
+    expect(nameInput.props().value).toEqual('');
+  });
+
+  it("should clear birthday form", () => {
+    const birthInput = wrapperForm.find('#name');
+    birthInput.simulate('change', { target: { value: '10/10/1990' } });
+    expect(birthInput.props().value).toEqual('10/10/1990');
+    resetButton.simulate('click');
+    expect(birthInput.props().value).toEqual('');
+  });
+
+  it("should clear first password form", () => {
+    const passInput = wrapperForm.find('#name');
+    passInput.simulate('change', { target: { value: '123456' } });
+    expect(passInput.props().value).toEqual('123456');
+    resetButton.simulate('click');
+    expect(passInput.props().value).toEqual('');
+  });
+
+  it("should clear confirm password form", () => {
+    const confirmInput = wrapperForm.find('#name');
+    confirmInput.simulate('change', { target: { value: '123456' } });
+    expect(confirmInput.props().value).toEqual('123456');
+    resetButton.simulate('click');
+    expect(confirmInput.props().value).toEqual('');
+  });
+
 });
+
+
