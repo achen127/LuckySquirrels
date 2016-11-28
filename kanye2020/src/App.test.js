43cc2a21535ee3abe2b42696b3bf503f481acec2
sarahfeldmann <sarahf95@gmail.com>
2016-11-27 23:55:15 -0800

    Add outline of submit button tests

--- kanye2020/src/App.test.js
+++ kanye2020/src/App.test.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import SignUpForm from './TeamSignup';
+import {shallow, mount} from 'enzyme';
+import sinon from 'sinon';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+// Form submit button tests
+describe('Submit Form Button', () => {
+  
+  it('checks name is valid', () =>{
+
+  });
+
+  it('checks birthdate is valid', () =>{
+
+  });
+
+  it('checks password is valid', () => {
+
+  });
+
+  it('checks if submit is enabled when form is valid', () => {
+
+  })
+
 });
